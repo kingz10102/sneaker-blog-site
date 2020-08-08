cc0332a4ac84d99e4d4fb5c5848a5779c889a349
Kok Kok <REMY@Koks-MacBook-Pro.local>
2020-08-08 12:58:38 -0400

    this a  commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import  Avatar  from '@material-ui/core/Avatar';
+
+
+
 
 
 const useStyles = makeStyles((theme) => ({
@@ -12,7 +22,7 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: '#fff'
   },
   sneaker: {
-    backgroundImage: `linear-gradient(rgba(0,0,0.5), rgba(0,0,0.5)), url('https://images.unsplash.com/photo-1520316587275-5e4f06f355e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')`,
+    backgroundImage: `linear-gradient(rgba(0,0,0.5), rgba(0,0,0.5)), url('https://www.istockphoto.com/photo/great-sneaker-gm1079117394-289156348')`,
     height: '500px',
     backgroundPosition: 'center',
     backgroundRepeat: 'no-repeat',
@@ -31,6 +41,20 @@ const useStyles = makeStyles((theme) => ({
   blogTitle: {
     fontWeight: 800,
     paddingBottom: theme.spacing(3)
+  },
+  card: {
+    maxWidth: "100%",
+  },
+  media: {
+    height: 240
+  },
+  cardActions: {
+    display: "flex",
+    margin: "0 10px",
+    justifyContent: "space-between"
+  },
+  author: {
+    display: "flex"
   }
 }));
 
@@ -42,7 +66,7 @@ function App() {
       <AppBar className={classes.appBar} position="static">
         <Toolbar>
           <Typography variant="h6" color='primary'>
-             KICKZZ Blog
+            KICKZZ Blog
           </Typography>
         </Toolbar>
       </AppBar>
@@ -53,7 +77,226 @@ function App() {
         <Typography variant="h4" className={classes.blogTitle}>
           Articles 
         </Typography>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6} md={4}>
+          <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="https://media.istockphoto.com/photos/great-sneaker-picture-id1079117394"
+          title="istock sneaker photo"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Ambitious Sneakers
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions className={classes.cardActions}>
+          <Box className={classes.author}>
+                  <Avatar src="https://media.istockphoto.com/photos/3d-basketball-drinks-at-the-party-picture-id522344211" />
+                  <Box ml={2}>
+                    <Typography variant="subtitle2" component="p">
+                      BALL GOD
+                    </Typography>
+                    <Typography variant="subtitle2" color="textSecondary" component="p">
+                    March 11, 2020
+                    </Typography>
+                  </Box>
+                </Box>
+                <Box>
+                 
+                </Box>
+      </CardActions>
+    </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+          <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="https://images.pexels.com/photos/2729899/pexels-photo-2729899.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+          title="istock sneaker photo"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Ambitious Sneakers
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          Accumsan bibendum dictumst egestas facilisis fringilla litora mi non per potenti quisque rhoncus risus sociis ultrices ut vivamis suscipit ultricies venenatis.
+  luctus nec nisl odio per primis proin quis torquent tristique. penatibus posuere praesent quis quisque sagittis scelerisque semper suspendisse tristique ullamcorper vehicula volutpat.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions className={classes.cardActions}>
+          <Box className={classes.author}>
+                  <Avatar src="https://images.pexels.com/photos/3152045/pexels-photo-3152045.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+                  <Box ml={2}>
+                    <Typography variant="subtitle2" component="p">
+                      Crossover Prince
+                    </Typography>
+                    <Typography variant="subtitle2" color="textSecondary" component="p">
+                    March 11, 2020
+                    </Typography>
+                  </Box>
+                </Box>
+                <Box>
+                 
+                </Box>
+      </CardActions>
+    </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+          <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="https://images.pexels.com/photos/2385477/pexels-photo-2385477.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+          title="istock sneaker photo"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Ambitious Sneakers
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions className={classes.cardActions}>
+          <Box className={classes.author}>
+                  <Avatar src="https://images.pexels.com/photos/2834917/pexels-photo-2834917.jpeg?cs=srgb&dl=pexels-wallace-chuck-2834917.jpg&fm=jpg" />
+                  <Box ml={2}>
+                    <Typography variant="subtitle2" component="p">
+                      Alley-OPP
+                    </Typography>
+                    <Typography variant="subtitle2" color="textSecondary" component="p">
+                    March 11, 2020
+                    </Typography>
+                  </Box>
+                </Box>
+                <Box>
+                 
+                </Box>
+      </CardActions>
+    </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+          <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="https://images.pexels.com/photos/609771/pexels-photo-609771.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+          title="istock sneaker photo"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Ambitious Sneakers
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions className={classes.cardActions}>
+          <Box className={classes.author}>
+                  <Avatar src="https://images.pexels.com/photos/3686769/pexels-photo-3686769.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" />
+                  <Box ml={2}>
+                    <Typography variant="subtitle2" component="p">
+                      BALL GOD
+                    </Typography>
+                    <Typography variant="subtitle2" color="textSecondary" component="p">
+                    March 11, 2020
+                    </Typography>
+                  </Box>
+                </Box>
+                <Box>
+                 
+                </Box>
+      </CardActions>
+    </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+          <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="https://images.pexels.com/photos/1192043/pexels-photo-1192043.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+          title="istock sneaker photo"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Ambitious Sneakers
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          Accumsan bibendum dictumst egestas facilisis fringilla litora mi non per potenti quisque rhoncus risus sociis ultrices ut vivamis suscipit ultricies venenatis.
+  luctus nec nisl odio per primis proin quis torquent tristique. penatibus posuere praesent quis quisque sagittis scelerisque semper suspendisse tristique ullamcorper vehicula volutpat.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions className={classes.cardActions}>
+          <Box className={classes.author}>
+                  <Avatar src="https://media.istockphoto.com/photos/3d-basketball-drinks-at-the-party-picture-id522344211" />
+                  <Box ml={2}>
+                    <Typography variant="subtitle2" component="p">
+                      BALL GOD
+                    </Typography>
+                    <Typography variant="subtitle2" color="textSecondary" component="p">
+                    March 11, 2020
+                    </Typography>
+                  </Box>
+                </Box>
+                <Box>
+                 
+                </Box>
+      </CardActions>
+    </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+          <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="https://media.istockphoto.com/photos/great-sneaker-picture-id1079117394"
+          title="istock sneaker photo"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Ambitious Sneakers
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions className={classes.cardActions}>
+          <Box className={classes.author}>
+                  <Avatar src="https://media.istockphoto.com/photos/3d-basketball-drinks-at-the-party-picture-id522344211" />
+                  <Box ml={2}>
+                    <Typography variant="subtitle2" component="p">
+                      BALL GOD
+                    </Typography>
+                    <Typography variant="subtitle2" color="textSecondary" component="p">
+                    March 11, 2020
+                    </Typography>
+                  </Box>
+                </Box>
+                <Box>
+                 
+                </Box>
+      </CardActions>
+    </Card>
+          </Grid>
+        </Grid>
       </Container>
+
     </div>
   );
 }
